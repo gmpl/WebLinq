52390653070d1483ccee310eabbf758753a66cf9
Atif Aziz <code@raboof.com>
2016-11-14 10:53:35 +0100

    Conclude fix

--- eg/Program.cs
+++ eg/Program.cs
@@ -145,7 +145,7 @@ namespace WebLinq.Samples
         static void Dump<T>(this IQuery<T> query, TextWriter output = null)
         {
             output = output ?? Console.Out;
-            foreach (var e in query.ToTerminalEnumerable(DefaultQueryContext.Create))
+            foreach (var e in query.ToEnumerable(DefaultQueryContext.Create))
                 output.WriteLine(e);
         }
     }
